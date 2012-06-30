12631d61b709e787c2f51566c85756431e8d3066
statler <pon3yy@gmail.com>
2012-06-30 17:02:32 +0200

    Ajout d'un paramètre à load()

--- src/core/plugins/Plugin.java
+++ src/core/plugins/Plugin.java
@@ -1,8 +1,11 @@
 package core.plugins;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public interface Plugin
 {
-	public void lancer();
+	public void lancer(ArrayList<File> listeFichiersSelectionnes);
 	public void stopper();
 	public String getNom();
 	public boolean isActive();
