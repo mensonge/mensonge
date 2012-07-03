15c5c1359cd736679ae1d0ad0b021becda34645a
Mi-black <6mi6black6@gmail.com>
2012-07-03 10:19:26 +0200

    Creation du fichier LecteurVideo

---
+++ src/userinterface/LecteurVideo.java
@@ -0,0 +1,36 @@
+package userinterface;
+
+
+public class LecteurVideo
+{
+	private File fichierVideo;
+
+	public LecteurVideo(File fichierVideo)
+	{
+		this.fichierVideo = fichierVideo;
+	}
+
+	public void initialiserComposant()
+	{
+
+
+	}
+
+	public void play()
+	{
+	}
+
+	public void pause()
+	{
+
+	}
+
+	public void stop()
+	{
+	}
+
+	public void setVolume(long volume)
+	{
+	}
+
+}
