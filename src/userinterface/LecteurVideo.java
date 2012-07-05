c810b50d404adc390b8f351ea5f89d5bd35cc4ac
Mi-black <6mi6black6@gmail.com>
2012-07-06 01:48:40 +0200

    création du squelette de LecteurVideo

--- src/userinterface/LecteurVideo.java
+++ src/userinterface/LecteurVideo.java
@@ -1,18 +1,31 @@
 package userinterface;
 
+import com.xuggle.mediatool.IMediaReader;
+import com.xuggle.mediatool.MediaListenerAdapter;
+import com.xuggle.mediatool.ToolFactory;
+import com.xuggle.mediatool.event.IVideoPictureEvent;
+import com.xuggle.xuggler.Global;
 
 public class LecteurVideo
 {
 	private File fichierVideo;
+	private String nom;
+
+
+	IMediaReader mediaReader;
 
 	public LecteurVideo(File fichierVideo)
 	{
 		this.fichierVideo = fichierVideo;
+		this.nom = fichierVideo.getName();
+		this.initialiserComposant();
 	}
 
 	public void initialiserComposant()
 	{
-
+		mediaReader	= ToolFactory.makeReader(nom);
+		mediaReader.setBufferedImageTypeToGenerate(BufferedImage.TYPE_3BYTE_BGR);
+		mediaReader.addListener(new ImageSnapListener();
 
 	}
 
