a9229432fb2a05b3d378eae3f571f075ffe104a9
statler <pon3yy@gmail.com>
2012-08-19 17:46:36 +0200

    Création du plugin des coeff cepstraux

--- plugins/coeffCepstraux/src/core/CoefficientsCepstraux.java
+++ plugins/coeffCepstraux/src/core/CoefficientsCepstraux.java
@@ -3,14 +3,26 @@ package core;
 import core.plugins.Plugin;
 import java.io.File;
 import java.util.ArrayList;
+import core.Extraction;
 
 public class CoefficientsCepstraux implements Plugin
 {
 	private boolean isActive = false;
 
-	public void lancer(ArrayList<File> listeFichiersSelectionnes)
+	public void lancer(ArrayList<File> fichiers)
 	{
 		this.isActive = true;
+		if(fichiers.isEmpty())
+		{
+			System.out.println("[E] Aucun fichier sélectionné !");
+		}
+		else
+		{
+			double echs[][] = Extraction.extraireEchantillons(fichiers.get(0));
+			System.out.println(echs[12000][0]+" "+echs[12000][1]);
+		}
+
+		this.isActive = false;
 	}
 	public void stopper()
 	{
@@ -23,5 +35,13 @@ public class CoefficientsCepstraux implements Plugin
 	public boolean isActive()
 	{
 		return isActive;
+	}
+	public static void main(String args[])
+	{
+		ArrayList<File> fichiers = new ArrayList<File>();
+		fichiers.add(new File("sons/test.wav"));
+		CoefficientsCepstraux coeff = new CoefficientsCepstraux();
+		System.out.println("[i] "+coeff.getNom());
+		coeff.lancer(fichiers);
 	}
 }
