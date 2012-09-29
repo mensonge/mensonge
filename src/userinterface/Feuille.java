56efbeab01572d7478780013f42d6575cdb12f98
Grenth <tarte@medrengard.home>
2012-09-29 16:37:09 +0000

    creation du consstructeur et des attributs.

--- src/userinterface/Feuille.java
+++ src/userinterface/Feuille.java
@@ -4,5 +4,17 @@ import javax.swing.tree.DefaultMutableTreeNode;
 
 public class Feuille extends DefaultMutableTreeNode
 {
-
+	int id;
+	String nom;
+	int duree;
+	int taille;
+	
+	public Feuille(int id, String nom, int duree, int taille)
+	{
+		super(nom);
+		this.id = id;
+		this.duree = duree;
+		this.taille = taille;
+		this.nom = nom;
+	}
 }
