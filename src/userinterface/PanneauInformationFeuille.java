ef511ea06035967a259457542a181e67a5c40d15
Paymon <akkarin@hotmail.fr>
2012-10-01 10:03:54 +0200

    Ajout de la classe pour gérer l'affichage des fichier dans l'arbre

---
+++ src/userinterface/PanneauInformationFeuille.java
@@ -0,0 +1,52 @@
+package userinterface;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import javax.swing.JPanel;
+
+public class PanneauInformationFeuille extends JPanel
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	Map<String, String> listeInfo = null;
+	
+	public void paint(Graphics g)
+	{
+		int i = 0;
+		Font font = new Font("Courier", Font.BOLD, 15);
+	    g.setFont(font);
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, this.getWidth(), this.getHeight());
+		g.setColor(Color.BLACK);
+		if(listeInfo != null)
+		{
+			Set<String> listeKey = listeInfo.keySet();
+			Iterator<String> iterator = listeKey.iterator();
+			while(iterator.hasNext())
+			{
+				int y = 20 + 20*i;
+				int x = 10;
+				String current = (String) iterator.next();
+				g.drawString(current + " : " + listeInfo.get(current), x, y);
+				i++;
+			}
+		}
+	}
+
+	public Map<String, String> getListeInfo() {
+		return listeInfo;
+	}
+
+	public void setListeInfo(Map<String, String> listeInfo) {
+		this.listeInfo = listeInfo;
+	}
+	
+}
+
