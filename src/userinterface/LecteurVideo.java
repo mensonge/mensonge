aa0e96ac65d333ee7512731fa817991857ef558f
m0narch <6mi6black6@gmail.com>
2012-11-14 22:51:29 +0100

    Ajout de fonctionalité :Defilement de la barre de progression et mise a jour du temps courant

--- src/userinterface/LecteurVideo.java
+++ src/userinterface/LecteurVideo.java
@@ -1,39 +1,14 @@
 package userinterface;
 
 import com.xuggle.mediatool.IMediaReader;
-import com.xuggle.mediatool.MediaListenerAdapter;
-import com.xuggle.mediatool.ToolFactory;
-import com.xuggle.mediatool.event.IVideoPictureEvent;
-import com.xuggle.mediatool.event.VideoPictureEvent;
-import com.xuggle.mediatool.event.IAudioSamplesEvent;
-import com.xuggle.xuggler.Global;
-import com.xuggle.xuggler.IContainer;
-import com.xuggle.xuggler.IStream;
-import com.xuggle.xuggler.IAudioSamples;
-import com.xuggle.xuggler.IContainer;
-import com.xuggle.xuggler.IStreamCoder;
-import com.xuggle.xuggler.ICodec;
-import com.xuggle.xuggler.IMetaData;
-import com.xuggle.xuggler.IVideoPicture;
-import com.xuggle.xuggler.IVideoResampler;
-
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.DataLine;
-import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.ShortBuffer;
-
-import java.awt.Color;
 import java.awt.BorderLayout;
-import java.awt.image.BufferedImage;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import java.util.concurrent.TimeUnit;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import javax.swing.JPanel;
@@ -44,10 +19,12 @@ import javax.swing.ImageIcon;
 import javax.swing.JToolBar;
 import javax.swing.plaf.metal.MetalSliderUI;
 
+import uk.co.caprica.vlcj.binding.internal.libvlc_media_t;
 import uk.co.caprica.vlcj.component.EmbeddedMediaPlayerComponent;
+import uk.co.caprica.vlcj.player.MediaPlayer;
+import uk.co.caprica.vlcj.player.MediaPlayerEventListener;
 
-public class LecteurVideo extends JPanel implements ActionListener
-{
+public class LecteurVideo extends JPanel implements ActionListener {
 	private static final long serialVersionUID = 5373991180139317820L;
 	private File fichierVideo;
 	private String nom;
@@ -64,41 +41,33 @@ public class LecteurVideo extends JPanel implements ActionListener
 	private ImageComponent mediaPlayerComponent;
 	private IMediaReader reader;
 	private long duration;
-	private double volume; //Entre 0 et 1
+	private int volume; // Entre 0 et 100
 	private boolean pause;
 	private boolean stop;
 	private SourceDataLine mLine;
-	private   EmbeddedMediaPlayerComponent vidComp;
+	private EmbeddedMediaPlayerComponent vidComp;
 
-
-	public LecteurVideo(final File fichierVideo)
-	{
-		this.volume = 1d;
+	public LecteurVideo(final File fichierVideo) {
+		this.volume = 75;
 		this.pause = true;
 		this.stop = true;
 		this.fichierVideo = fichierVideo;
-		try
-		{
+		try {
 			this.nom = fichierVideo.getCanonicalPath();
-		}
-		catch (IOException e)
-		{
-			GraphicalUserInterface.popupErreur(e.getMessage(),"Erreur");
+		} catch (IOException e) {
+			GraphicalUserInterface.popupErreur(e.getMessage(), "Erreur");
 		}
 
 		initialiserComposants();
-		javax.swing.SwingUtilities.invokeLater(new Runnable()
-		{
-			public void run()
-			{	
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
 				vidComp.getMediaPlayer().playMedia(nom);
 				ouvrirVideo();
 			}
-	});
+		});
 	}
 
-	private void initialiserComposants()
-	{
+	private void initialiserComposants() {
 
 		this.imageIconPause = new ImageIcon("images/Pause.png");
 		this.imageIconStop = new ImageIcon("images/Stop.png");
@@ -108,8 +77,8 @@ public class LecteurVideo extends JPanel implements ActionListener
 		this.labelDureeMax = new JLabel("00:00:00");
 
 		this.panelDuree = new JPanel(new BorderLayout());
-		this.panelDuree.add(labelDureeActuelle,BorderLayout.WEST);
-		this.panelDuree.add(labelDureeMax,BorderLayout.EAST);
+		this.panelDuree.add(labelDureeActuelle, BorderLayout.WEST);
+		this.panelDuree.add(labelDureeMax, BorderLayout.EAST);
 
 		this.boutonLecture = new JButton();
 		this.boutonLecture.setToolTipText("Lancer");
@@ -128,42 +97,39 @@ public class LecteurVideo extends JPanel implements ActionListener
 		this.sliderVolume.setPaintLabels(false);
 		this.sliderVolume.setMinimum(0);
 		this.sliderVolume.setMaximum(100);
-		this.sliderVolume.setValue(100);
+		this.sliderVolume.setValue(75);
 		this.sliderVolume.setToolTipText("Volume");
-		this.sliderVolume.addChangeListener(new ChangeListener()
-				{
-					public void stateChanged(ChangeEvent e)
-					{
-						volume = ((double)sliderVolume.getValue())/100;
-					}
-				});
+		this.sliderVolume.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				volume = sliderVolume.getValue();
+				vidComp.getMediaPlayer().setVolume(sliderVolume.getValue());
+				// System.out.println(sliderVolume.getValue());
+			}
+		});
 
 		this.slider = new JSlider(JSlider.HORIZONTAL);
 		this.slider.setPaintTicks(false);
 		this.slider.setPaintLabels(false);
 		this.slider.setMinimum(0);
 		this.slider.setValue(0);
-		this.slider.setUI(new MetalSliderUI()
-		{
-			protected void scrollDueToClickInTrack(int direction)
-			{
-				//On pourra récup les millisecondes pour l'extraction de cette façon aussi
-				//FIXME : segfault pour le moment
-				/*
-				 *int value = slider.getValue();
-				 *if (slider.getOrientation() == JSlider.HORIZONTAL)
-				 *{
-				 *    value = this.valueForXPosition(slider.getMousePosition().x);
-				 *}
-				 *else if (slider.getOrientation() == JSlider.VERTICAL)
-				 *{
-				 *    value = this.valueForYPosition(slider.getMousePosition().y);
-				 *}
-				 *reader.getContainer().seekKeyFrame(videoStreamIndex,(long) value,0);
-				 *slider.setValue(value);
-				 */
+		/*
+		 * this.slider.setUI(new MetalSliderUI() { protected void
+		 * scrollDueToClickInTrack(int direction) { //On pourra récup les
+		 * millisecondes pour l'extraction de cette façon aussi //FIXME :
+		 * segfault pour le moment
+		 * 
+		 * int value = slider.getValue(); if (slider.getOrientation() ==
+		 * JSlider.HORIZONTAL) { value =
+		 * this.valueForXPosition(slider.getMousePosition().x); } //
+		 * vidComp.getMediaPlayer().setPosition(value/); slider.setValue(value);
+		 * } });
+		 */
+		/*this.slider.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				vidComp.getMediaPlayer().setTime(slider.getValue() / 100);
+
 			}
-		});
+		});*/
 
 		JToolBar toolBar = new JToolBar();
 		toolBar.setFloatable(false);
@@ -175,136 +141,281 @@ public class LecteurVideo extends JPanel implements ActionListener
 
 		vidComp = new EmbeddedMediaPlayerComponent();
 		vidComp.setVisible(true);
+		vidComp.getMediaPlayer().addMediaPlayerEventListener(
+				new PlayerEventListener());
+
 		this.setLayout(new BorderLayout());
-		this.add(vidComp,BorderLayout.CENTER);
+		this.add(vidComp, BorderLayout.CENTER);
 		this.add(toolBar, BorderLayout.SOUTH);
 	}
-	
-	private void ouvrirVideo()
-	{
-		//this.reader.open();
-		this.duration = reader.getContainer().getDuration()/1000;//C'est en microsecondes, on met en milisecondes
-		this.slider.setMaximum((int)this.duration);
 
-		long duree = duration/1000;
-		int heures = (int) (duree/3600);
-		int minutes = (int) ((duree%3600)/60);
-		int secondes = (int) ((duree%3600)%60);
-		labelDureeMax.setText(String.format("%02d:%02d:%02d", heures, minutes, secondes));
-	}
-	
-	private void ouvrirAudio()
-	{
-		/*if(this.reader.isOpen())
-		{
-			IContainer container = reader.getContainer();
-			IStreamCoder audioCoder = null;
-			int numStreams = container.getNumStreams();
-			int audioStreamId = -1;
-			for(int i = 0; i < numStreams; i++)
-			{
-				IStream stream = container.getStream(i);
-				IStreamCoder coder = stream.getStreamCoder();
-				if(coder.getCodecType() == ICodec.Type.CODEC_TYPE_AUDIO)
-				{
-					audioStreamId = i;
-					audioCoder = coder;
-					break;
-				}
-			}
-			if(audioStreamId == -1)
-			{
-				throw new RuntimeException("could not find audio stream");
-			}
-			IMetaData options = IMetaData.make();
-			IMetaData unsetOptions = IMetaData.make();
-			if(audioCoder.open(options,unsetOptions) < 0)
-			{
-				throw new RuntimeException("could not open audio decoder");
-			}
-			AudioFormat audioFormat = new AudioFormat(audioCoder.getSampleRate(),
-					(int)IAudioSamples.findSampleBitDepth(audioCoder.getSampleFormat()),
-					audioCoder.getChannels(),
-					true, /* xuggler defaults to signed 16 bit samples 
-					false);
-			DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat);
-			try
-			{
-				this.mLine = (SourceDataLine) AudioSystem.getLine(info);
-				this.mLine.open(audioFormat);
-				this.mLine.start();
-			}
-			catch(LineUnavailableException e)
-			{
-				throw new RuntimeException("could not open audio line");
-			}
-		}*/
+	private void ouvrirVideo() {
+		try {
+			Thread.sleep(750);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		this.pause = false;
+		this.boutonLecture.setIcon(imageIconPause);
+		this.boutonLecture.setToolTipText("Mettre en pause");
+		vidComp.getMediaPlayer().setVolume(sliderVolume.getValue());
+		this.duration = vidComp.getMediaPlayer().getLength() / 1000;
+		this.slider.setMaximum(100);
+		long duree = duration;
+		int heures = (int) (duree / 3600);
+		int minutes = (int) ((duree % 3600) / 60);
+		int secondes = (int) ((duree % 3600) % 60);
+		labelDureeMax.setText(String.format("%02d:%02d:%02d", heures, minutes,secondes));
 	}
 
-	public void play()
-	{
-		this.vidComp.getMediaPlayer().pause();
+	/*
+	 * private void ouvrirAudio() { if(this.reader.isOpen()) { IContainer
+	 * container = reader.getContainer(); IStreamCoder audioCoder = null; int
+	 * numStreams = container.getNumStreams(); int audioStreamId = -1; for(int i
+	 * = 0; i < numStreams; i++) { IStream stream = container.getStream(i);
+	 * IStreamCoder coder = stream.getStreamCoder(); if(coder.getCodecType() ==
+	 * ICodec.Type.CODEC_TYPE_AUDIO) { audioStreamId = i; audioCoder = coder;
+	 * break; } } if(audioStreamId == -1) { throw new
+	 * RuntimeException("could not find audio stream"); } IMetaData options =
+	 * IMetaData.make(); IMetaData unsetOptions = IMetaData.make();
+	 * if(audioCoder.open(options,unsetOptions) < 0) { throw new
+	 * RuntimeException("could not open audio decoder"); } AudioFormat
+	 * audioFormat = new AudioFormat(audioCoder.getSampleRate(),
+	 * (int)IAudioSamples.findSampleBitDepth(audioCoder.getSampleFormat()),
+	 * audioCoder.getChannels(), true, /* xuggler defaults to signed 16 bit
+	 * samples false); DataLine.Info info = new
+	 * DataLine.Info(SourceDataLine.class, audioFormat); try { this.mLine =
+	 * (SourceDataLine) AudioSystem.getLine(info); this.mLine.open(audioFormat);
+	 * this.mLine.start(); } catch(LineUnavailableException e) { throw new
+	 * RuntimeException("could not open audio line"); } } }
+	 */
+
+	public void play() {
+		if (this.vidComp.getMediaPlayer().isPlaying()) {
+			this.vidComp.getMediaPlayer().pause();
+		} else
+			this.vidComp.getMediaPlayer().play();
 		this.pause = false;
 		this.stop = false;
 		this.boutonLecture.setIcon(imageIconPause);
 		this.boutonLecture.setToolTipText("Mettre en pause");
-		
 	}
 
-	public void pause()
-	{
+	public void pause() {
 		this.vidComp.getMediaPlayer().pause();
 		this.pause = true;
 		this.boutonLecture.setIcon(imageIconLecture);
 		this.boutonLecture.setToolTipText("Lancer");
 	}
 
-	public void stop()
-	{
+	public void stop() {
+		this.vidComp.getMediaPlayer().stop();
 		this.stop = true;
 		this.pause = true;
-		if(this.reader.isOpen())
-		{
-			this.reader.close();
-			this.mLine.close();
-		}
 		this.slider.setValue(0);
-		this.labelDureeActuelle.setText("00:00:00");
-		this.labelDureeMax.setText("00:00:00");
+		// this.labelDureeActuelle.setText("00:00:00");
+		// this.labelDureeMax.setText("00:00:00");
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent event)
-	{
-		if(event.getSource() == boutonLecture)
-		{
-			if(this.pause == true)
-			{
+	public void actionPerformed(ActionEvent event) {
+		if (event.getSource() == boutonLecture) {
+			if (this.pause == true) {
 				this.play();
-			}
-			else
-			{
+			} else {
 				this.pause();
 			}
-		}
-		else if(event.getSource() == boutonStop)
-		{
-			if(!this.stop)
-			{
-				if(this.pause == true)
-				{
+		} else if (event.getSource() == boutonStop) {
+			if (!this.stop) {
+				if (this.pause == true) {
 					this.boutonLecture.setIcon(imageIconPause);
 					this.boutonLecture.setToolTipText("Mettre en pause");
-				}
-				else
-				{
+				} else {
 					this.boutonLecture.setIcon(imageIconLecture);
 					this.boutonLecture.setToolTipText("Lancer");
 				}
 				this.stop();
-				mediaPlayerComponent.setImage(null);
-				mediaPlayerComponent.repaint();
 			}
 		}
 	}
+
+	private class PlayerEventListener implements MediaPlayerEventListener {
+
+		@Override
+		public void backward(MediaPlayer arg0) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void buffering(MediaPlayer arg0, float arg1) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void endOfSubItems(MediaPlayer arg0) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void error(MediaPlayer arg0) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void finished(MediaPlayer arg0) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void forward(MediaPlayer arg0) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void lengthChanged(MediaPlayer arg0, long arg1) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void mediaChanged(MediaPlayer arg0, libvlc_media_t arg1,
+				String arg2) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void mediaDurationChanged(MediaPlayer arg0, long arg1) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void mediaFreed(MediaPlayer arg0) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void mediaMetaChanged(MediaPlayer arg0, int arg1) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void mediaParsedChanged(MediaPlayer arg0, int arg1) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void mediaStateChanged(MediaPlayer arg0, int arg1) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void mediaSubItemAdded(MediaPlayer arg0, libvlc_media_t arg1) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void newMedia(MediaPlayer arg0) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void opening(MediaPlayer arg0) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void pausableChanged(MediaPlayer arg0, int arg1) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void paused(MediaPlayer arg0) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void playing(MediaPlayer arg0) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void positionChanged(MediaPlayer arg0, float time) 
+		{
+			duration = arg0.getTime() / 1000;
+			long duree = duration;
+			int heures = (int) (duree / 3600);
+			int minutes = (int) ((duree % 3600) / 60);
+			int secondes = (int) ((duree % 3600) % 60);
+			labelDureeActuelle.setText(String.format("%02d:%02d:%02d", heures,minutes, secondes));
+			
+		}
+
+		@Override
+		public void seekableChanged(MediaPlayer arg0, int arg1) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void snapshotTaken(MediaPlayer arg0, String arg1) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void stopped(MediaPlayer arg0) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void subItemFinished(MediaPlayer arg0, int arg1) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void subItemPlayed(MediaPlayer arg0, int arg1) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void timeChanged(MediaPlayer arg0, long time) 
+		{ 
+			slider.setValue((int)(arg0.getPosition()*100));
+		}
+
+		@Override
+		public void titleChanged(MediaPlayer arg0, int arg1) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void videoOutput(MediaPlayer arg0, int arg1) {
+			// TODO Auto-generated method stub
+
+		}
+	}
+
 }
