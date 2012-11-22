3e5a2bb3255d71f22e3f6aa604e9bd53a3683fa5
m0narch <6mi6black6@gmail.com>
2012-11-22 14:00:15 +0100

    Commit pour réparer l'erreur de l'autre

---
+++ src/userinterface/Branche.java
@@ -0,0 +1,27 @@
+package userinterface;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+
+public class Branche extends DefaultMutableTreeNode
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	/**
+	 * Constructeur par défaut
+	 */
+	public Branche()
+	{
+		super();
+	}
+	
+	/**
+	 * Constructeur avec nom
+	 * @param nom nom de l'objet
+	 */
+	public Branche(String nom)
+	{
+		super(nom);
+	}
+}
