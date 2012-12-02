78d77e2b22f5104668875261ed3896cf71d2c815
statler <pon3yy@gmail.com>
2012-12-02 17:45:07 +0100

    Ajout d'une bibliothèque pour les graphes et début de classe pour aider
    DrawXYGraph va permettre de créer facilement des graphiques pour
    visualiser les résultats des calculs effectués

--- plugins/coeffCepstraux/src/main/java/coeffCepstraux/core/CoefficientsCepstraux.java
+++ plugins/coeffCepstraux/src/main/java/coeffCepstraux/core/CoefficientsCepstraux.java
@@ -3,17 +3,50 @@ package coeffCepstraux.core;
 import java.io.File;
 import java.util.List;
 
+import javax.swing.SwingUtilities;
+
 import mensonge.core.IExtraction;
 import mensonge.core.plugins.Plugin;
+
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+import coeffCepstraux.userinterface.DrawXYGraph;
 
 public class CoefficientsCepstraux implements Plugin
 {
 	private boolean isActive = false;
 
+	private void drawGraph()
+	{
+		XYSeries series = new XYSeries("Canard !");
+		final double phaseMultiplier = 2 * Math.PI * 5 / 500;
+
+		for (int i = 0; i < 500; i++)
+		{
+			final double cycleX = i * phaseMultiplier;
+			final double sineResult = Math.sin(cycleX);
+			series.add(cycleX, sineResult);
+		}
+		final XYDataset xyDataset = new XYSeriesCollection(series);
+		SwingUtilities.invokeLater(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+
+				DrawXYGraph graph = new DrawXYGraph("Canard ?", "Super graph", xyDataset);
+				graph.display();
+			}
+		});
+	}
+
 	@Override
 	public void lancer(IExtraction extraction, List<File> listeFichiersSelectionnes)
 	{
 		this.isActive = true;
+		this.drawGraph();
 		this.isActive = false;
 	}
 
@@ -34,5 +67,4 @@ public class CoefficientsCepstraux implements Plugin
 	{
 		return isActive;
 	}
-
 }
---
+++ plugins/coeffCepstraux/src/main/java/coeffCepstraux/userinterface/DrawXYGraph.java
@@ -0,0 +1,57 @@
+package coeffCepstraux.userinterface;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.StandardXYItemRenderer;
+import org.jfree.data.xy.XYDataset;
+
+public class DrawXYGraph extends JFrame
+{
+	private static final long serialVersionUID = 1L;
+	private XYPlot plot;
+	private int index;
+
+	public DrawXYGraph(String applicationTitle, String chartTitle, XYDataset xyDataset)
+	{
+		super(applicationTitle);
+		JFreeChart chart = ChartFactory.createXYLineChart(chartTitle, "Abscisse", "Ordonnée", xyDataset,
+				PlotOrientation.VERTICAL, true, true, true);
+		this.index = 1;
+		this.plot = (XYPlot) chart.getPlot();
+		ChartPanel chartPanel = new ChartPanel(chart);
+		chartPanel.setPreferredSize(new Dimension(500, 270));
+		this.setContentPane(chartPanel);
+	}
+
+	public DrawXYGraph(String applicationTitle, String chartTitle)
+	{
+		super(applicationTitle);
+		JFreeChart chart = ChartFactory.createXYLineChart(chartTitle, "Abscisse", "Ordonnée", null,
+				PlotOrientation.VERTICAL, true, true, true);
+		this.plot = (XYPlot) chart.getPlot();
+		this.index = 0;
+		ChartPanel chartPanel = new ChartPanel(chart);
+		chartPanel.setPreferredSize(new Dimension(500, 270));
+		this.setContentPane(chartPanel);
+	}
+
+	public void addDataset(XYDataset xyDataset)
+	{
+		this.plot.setDataset(index, xyDataset);
+		this.plot.setRenderer(index, new StandardXYItemRenderer());
+		index++;
+	}
+
+	public void display()
+	{
+		this.pack();
+		this.setVisible(true);
+	}
+}
