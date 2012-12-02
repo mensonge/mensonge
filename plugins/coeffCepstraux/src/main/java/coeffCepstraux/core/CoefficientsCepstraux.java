c372c0120539cb3a6b911698974b519a7138ec31
statler <pon3yy@gmail.com>
2012-12-02 18:48:36 +0100

    Ajout de l'affichage de la variation d'amplitude d'un fichier wav
    Pour le plugin coeffCepstruax

--- plugins/coeffCepstraux/src/main/java/coeffCepstraux/core/CoefficientsCepstraux.java
+++ plugins/coeffCepstraux/src/main/java/coeffCepstraux/core/CoefficientsCepstraux.java
@@ -1,8 +1,12 @@
 package coeffCepstraux.core;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.SwingUtilities;
 
 import mensonge.core.IExtraction;
@@ -16,27 +20,67 @@ import coeffCepstraux.userinterface.DrawXYGraph;
 
 public class CoefficientsCepstraux implements Plugin
 {
+	private static Logger logger = Logger.getLogger("coeffCepstraux");
 	private boolean isActive = false;
 
-	private void drawGraph()
+	private void drawGraph(final double[][] echantillons)
 	{
-		XYSeries series = new XYSeries("Canard !");
-		final double phaseMultiplier = 2 * Math.PI * 5 / 500;
+		/*
+		 * XYSeries series = new XYSeries("Canard !"); final double phaseMultiplier = 2 * Math.PI * 5 / 500;
+		 * 
+		 * for (int i = 0; i < 500; i++) { final double cycleX = i * phaseMultiplier; final double sineResult =
+		 * Math.sin(cycleX); series.add(cycleX, sineResult); }
+		 */
 
-		for (int i = 0; i < 500; i++)
-		{
-			final double cycleX = i * phaseMultiplier;
-			final double sineResult = Math.sin(cycleX);
-			series.add(cycleX, sineResult);
-		}
-		final XYDataset xyDataset = new XYSeriesCollection(series);
 		SwingUtilities.invokeLater(new Runnable()
 		{
 			@Override
 			public void run()
 			{
+				final DrawXYGraph graph = new DrawXYGraph("Canard ?", "Variation d'amplitudes");
+				Thread t1 = new Thread(new Runnable()
+				{
+					
+					@Override
+					public void run()
+					{
+						XYSeries series = new XYSeries("Canal " + 0);
+						for (int j = 0; j < echantillons.length; j++)
+						{
+							series.add(j, echantillons[j][0]);
 
-				DrawXYGraph graph = new DrawXYGraph("Canard ?", "Super graph", xyDataset);
+						}
+						XYDataset xyDataset = new XYSeriesCollection(series);
+						graph.addDataset(xyDataset);
+					}
+				});
+				Thread t2 = new Thread(new Runnable()
+				{
+					
+					@Override
+					public void run()
+					{
+						XYSeries series = new XYSeries("Canal " +1);
+						for (int j = 0; j < echantillons.length; j++)
+						{
+							series.add(j, echantillons[j][1]);
+
+						}
+						XYDataset xyDataset = new XYSeriesCollection(series);
+						graph.addDataset(xyDataset);
+					}
+				});
+				t1.run();
+				t2.run();
+				try
+				{
+					t1.join();
+					t2.join();
+				}
+				catch (InterruptedException e)
+				{
+					logger.log(Level.WARNING, e.getMessage());
+				}
 				graph.display();
 			}
 		});
@@ -46,7 +90,24 @@ public class CoefficientsCepstraux implements Plugin
 	public void lancer(IExtraction extraction, List<File> listeFichiersSelectionnes)
 	{
 		this.isActive = true;
-		this.drawGraph();
+		if (!listeFichiersSelectionnes.isEmpty())
+		{
+			File test = listeFichiersSelectionnes.get(0);
+			try
+			{
+				double[][] echantillons = extraction.extraireEchantillons(test.getCanonicalPath());
+				this.drawGraph(echantillons);
+
+			}
+			catch (IOException e)
+			{
+				logger.log(Level.WARNING, e.getMessage());
+			}
+			catch (UnsupportedAudioFileException e)
+			{
+				logger.log(Level.WARNING, e.getMessage());
+			}
+		}
 		this.isActive = false;
 	}
 
