09d3fd0fd65a2e45a8213a9533f4fb1be9c67271
Azazel <love@magoa.hell>
2012-12-13 19:30:05 +0100

    modif renderdefault

---
+++ src/main/java/mensonge/userinterface/PanneauArbreRenderDefault.java
@@ -0,0 +1,63 @@
+package mensonge.userinterface;
+
+import java.awt.Component;
+
+import javax.swing.ImageIcon;
+import javax.swing.JTree;
+import javax.swing.tree.DefaultTreeCellRenderer;
+
+/**
+ * Classe affichant des image dans l'arbre
+ * 
+ * @author Azazel
+ * 
+ */
+public class PanneauArbreRenderDefault extends DefaultTreeCellRenderer
+{
+
+	/**
+	 * Le serail id
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Image associée à la racine
+	 */
+	private ImageIcon dossier = null;
+
+	/**
+	 * Image associée aux caseArbre
+	 */
+	private ImageIcon enregistrement = null;
+
+	
+	/**
+	 * Constructeur par défaut faisant des verifications pour pallier à l'absence des fichiers
+	 */
+	public PanneauArbreRenderDefault()
+	{
+
+		this.dossier = new ImageIcon("images/directory_default.png");
+
+		this.enregistrement = new ImageIcon("images/file_default.png");
+
+		
+	}
+
+	@Override
+	public Component getTreeCellRendererComponent(JTree tree, Object value, boolean selected, boolean expanded,
+			boolean leaf, int row, boolean hasFocus)
+	{
+		super.getTreeCellRendererComponent(tree, value, selected, expanded, leaf, row, hasFocus);
+		if(value instanceof Feuille)
+		{
+			this.setIcon(this.enregistrement);
+		}
+		else
+		{
+			this.setIcon(this.dossier);
+		}
+		return this;
+	}
+
+}
